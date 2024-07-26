19aaef1b585a379738e4ba7ec177b36919802503
DucTrung611 <thanductrung123@gmail.com>
2024-07-26 09:34:01 +0700

    .

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+
+export class CreateUserDto {
+    @IsEmail({}, { message: 'Email khong dung dinh dang', })
+    @IsNotEmpty({ message: 'email khong duoc de trong', })
+    email: string;
+
+    @IsNotEmpty({ message: 'password khong duoc de trong', })
+    password: string;
+    name: string;
+    address: string;
+}
